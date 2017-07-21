eeef13525429b2bc6af0c3f362b43b62a10d8dff
Michal Kaptur <kaptur.michal@gmail.com>
2017-07-21 23:08:19 +0200

    mod: force clang-format4

--- tools/format.sh
+++ tools/format.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 files="$(find src/ -name *.cpp -or -name *.h)"
-clang-format -i $files
+clang-format-4.0 -i $files
 
--- tools/is_formatted.sh
+++ tools/is_formatted.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 files="$(find src/ -name *.cpp -or -name *.h)"
-malformat_count=$(clang-format $files -output-replacements-xml | grep -c "<replacement ")
-clang-format -i $files
+malformat_count=$(clang-format-4.0 $files -output-replacements-xml | grep -c "<replacement ")
+clang-format-4.0 -i $files
 git status
 git diff
 clang-format --version
