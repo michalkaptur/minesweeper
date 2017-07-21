223e08bec4b9874e3d8b5665e2725afc9ec40ac7
Michal Kaptur <kaptur.michal@gmail.com>
2017-07-21 22:41:15 +0200

    add: debug info for formatting on CI

--- tools/is_formatted.sh
+++ tools/is_formatted.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 files="$(find src/ -name *.cpp -or -name *.h)"
 malformat_count=$(clang-format $files -output-replacements-xml | grep -c "<replacement ")
+clang-format -i $files
+git status
+git diff
+clang-format --version
 exit $malformat_count
 
