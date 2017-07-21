a47e54fe2efad9a27ca78cbd5a7be30d03b858a4
Michal Kaptur <kaptur.michal@gmail.com>
2017-07-21 23:16:18 +0200

    rm: debug

--- tools/is_formatted.sh
+++ tools/is_formatted.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 files="$(find src/ -name *.cpp -or -name *.h)"
 malformat_count=$(clang-format-4.0 $files -output-replacements-xml | grep -c "<replacement ")
-clang-format-4.0 -i $files
-git status
-git diff
-clang-format --version
 exit $malformat_count
 
