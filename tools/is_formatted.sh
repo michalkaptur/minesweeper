2356f430e2053f25120e363906b92cd6eaf47cc7
Michal Kaptur <kaptur.michal@gmail.com>
2018-08-24 22:27:22 +0200

    add: show formatting outcome

--- tools/is_formatted.sh
+++ tools/is_formatted.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 files="$(find src/ -name *.cpp -or -name *.h)"
 malformat_count=$(clang-format-4.0 $files -output-replacements-xml | grep -c "<replacement ")
+clang-format-4.0 $files -output-replacements-xml
 exit $malformat_count
 
